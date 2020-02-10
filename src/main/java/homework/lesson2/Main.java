11279792ed0110099a0618c61af9c0231a18b022
Chupin Pevel <pavel.chupin@gmail.com>
2020-02-10 09:34:27 +0700

    скорректирован пеодсчет времени в  ДЗ1 согласно замечания

---
+++ src/main/java/homework/lesson2/Main.java
@@ -0,0 +1,16 @@
+package homework.lesson2;
+
+import java.util.Random;
+
+public class Main {
+    public static int[] arr = new int[1000000];
+    static {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.random();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
