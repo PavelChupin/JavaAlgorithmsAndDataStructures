42d902fe0113a57dad1d021502f2888a0515b1b8
Chupin Pevel <pavel.chupin@gmail.com>
2020-02-21 17:54:57 +0700

    лекция

--- src/main/java/lesson/lesson5/Fibonacci.java
+++ src/main/java/lesson/lesson5/Fibonacci.java
@@ -8,7 +8,10 @@ package lesson.lesson5;
 * */
 
 public class Fibonacci {
-    public static int fibonacciRecursuion(int num){
+    static  long count;
+
+    public static long fibonacciRecursuion(int num){
+        count++;
         if (num == 0 ) return 0;
         if (num == 1 ) return 1;
 
@@ -16,8 +19,27 @@ public class Fibonacci {
         return fibonacciRecursuion(num - 1) + fibonacciRecursuion(num - 2);
     }
 
+    public static long fibonacci(int num){
+        long[] mem = new long[num + 1];
+        mem[0] = 0;
+        mem[1] = 1;
+
+        for (int i = 2; i <= num ; i++) {
+            count++;
+            mem[i] = mem[i-1] + mem[i-2];
+        }
+
+        return mem[num];
+    }
+
     public static void main(String[] args) {
-        System.out.println(fibonacciRecursuion(11));
+        count = 0;
+        System.out.println(fibonacciRecursuion(30));
+        System.out.println(count);
+
+        count = 0;
+        System.out.println(fibonacci(30));
+        System.out.println(count);
     }
 
 }
