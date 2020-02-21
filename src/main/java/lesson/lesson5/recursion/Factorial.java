9a1b5f2af5d595950ae58848fe8e78c4f6084719
Chupin Pevel <pavel.chupin@gmail.com>
2020-02-21 16:44:24 +0700

    лекция

---
+++ src/main/java/lesson/lesson5/recursion/Factorial.java
@@ -0,0 +1,29 @@
+package lesson.lesson5.recursion;
+
+
+public class Factorial {
+
+    public static long factorialRecursive(int num) {
+        if (num == 0) {
+            return 1;
+        }
+        return factorialRecursive(num - 1) * num;
+    }
+
+    public static long factorialLoop(int num) {
+        if (num == 0) {
+            return 1;
+        }
+
+        long res = 1;
+        for (int i = 1; i <= num; i++) {
+            res *= i;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorialRecursive(2));
+        System.out.println(factorialLoop(2));
+    }
+}
