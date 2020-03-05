73ace3b660b7b5d3907f0a2ef75d94256c83fc22
Chupin Pevel <pavel.chupin@gmail.com>
2020-03-05 12:01:35 +0700

    Создание кода ДЗ8

---
+++ src/main/java/homework/lesson8/Item.java
@@ -0,0 +1,13 @@
+package homework.lesson8;
+
+public class Item {
+        private int data;
+
+        public Item(int data){
+            this.data = data;
+        }
+
+        public int getKey(){
+            return this.data;
+        }
+}
